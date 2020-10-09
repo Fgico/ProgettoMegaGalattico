f0a13da0fda7ff5521e0d64e375960ab2ed2395d
Federico <Fgico@users.noreply.github.com>
2020-10-09 03:42:32 +0200

    Modificato Shader terreni
    Potete cambiare colori usati dei terreni creati proceduralmente aprendo lo shader shaderTerreniGenerati.tres e cambiando i colori sabbia roccia erba

--- Spatial.gd
+++ Spatial.gd
@@ -4,33 +4,34 @@ extends Spatial
 
 
 func _ready():
+	randomize()
 	var mesh = PlaneMesh.new()
-	mesh.material = preload("new_spatialmaterial.tres")
-	mesh.size = Vector2(1000,1000)
-	mesh.subdivide_depth = 100
-	mesh.subdivide_width = 100
+	mesh.material = preload("shaderTerreniGenerati.tres")
+	mesh.size = Vector2(200,200)
+	mesh.subdivide_depth = 200
+	mesh.subdivide_width = 200
 	
 	var noise = OpenSimplexNoise.new()
-	noise.seed = randi()
+	noise.seed = randi()			#casualizzo mappa
 	
 	var surfTool = SurfaceTool.new()
 	surfTool.create_from(mesh,0)
-	var arrplane = surfTool.commit()
+	var arrplane = surfTool.commit()		#prendo i punti del piano inizializzato
 	
 	var meshtool = MeshDataTool.new()
-	meshtool.create_from_surface(arrplane,0)
+	meshtool.create_from_surface(arrplane,0)		#glieli passo da gestire a meshtool
 
-	for i in range(meshtool.get_vertex_count()):
+	for i in range(meshtool.get_vertex_count()):		#prendo y da rumore
 		var v = meshtool.get_vertex(i)
-		v.y = noise.get_noise_2d(v.x,v.z) *100
+		v.y = noise.get_noise_2d(v.x,v.z) *30
 		meshtool.set_vertex(i, v)
 	
-	for i in range(arrplane.get_surface_count()):
+	for i in range(arrplane.get_surface_count()):		#rimuovo superfici create all'inizializzazione del piano per farne altre
 		arrplane.surface_remove(i)
 
 	meshtool.commit_to_surface(arrplane)
 	
-	surfTool.begin(mesh.PRIMITIVE_TRIANGLES)
+	surfTool.begin(mesh.PRIMITIVE_TRIANGLES)		#comincio a estrarre la mesh finale
 	surfTool.create_from(arrplane,0)
 	surfTool.generate_normals()
 	
@@ -38,9 +39,9 @@ func _ready():
 	inst.mesh = surfTool.commit()
 	inst.create_trimesh_collision()
 	
-	var packer = PackedScene.new()            #per salvare la mappa
-	packer.pack(inst)
-	ResourceSaver.save("res://terra.tscn", packer)
+	#var packer = PackedScene.new()            #per salvare la mappa
+	#packer.pack(inst)
+	#ResourceSaver.save("res://terra.tscn", packer)
 	add_child(inst)
 
 
