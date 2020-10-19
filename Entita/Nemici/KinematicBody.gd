b81f4bb5a2e9fe1622a85497e5606c2c3e659736
Federico Giusti <Fgico@users.noreply.github.com>
2020-10-19 21:12:56 +0200

    rripristin

---
+++ Entita/Nemici/KinematicBody.gd
@@ -0,0 +1,21 @@
+extends Combattente
+
+
+onready var nav = get_parent().get_node("Navigation")
+onready var player = get_parent().get_node("player")
+var path = []
+var direzione
+
+func _ready():
+	pass
+
+func _process(delta):
+	var arrivo = nav.get_closest_point(player.global_transform.origin)
+	var partenza = nav.get_closest_point(self.global_transform.origin)
+	path = nav.get_simple_path(partenza, arrivo)
+	direzione = path[1] - path[0]
+	direzione = direzione.normalized() * stats.spd * delta * 0.3
+	move_and_slide(direzione)
+	
+	
+
---
+++ GeneratoreTerreni.gd
@@ -0,0 +1,56 @@
+extends Spatial
+
+#generatore di mappe:
+
+
+func _ready():
+	randomize()
+	var mesh = PlaneMesh.new()
+	mesh.material = load("shaderTerreniGenerati.tres")
+	mesh.size = Vector2(200,200)
+	mesh.subdivide_depth = 200
+	mesh.subdivide_width = 200
+	
+	var noise = OpenSimplexNoise.new()
+	noise.seed = randi()			#casualizzo mappa
+	
+	var surfTool = SurfaceTool.new()
+	surfTool.create_from(mesh,0)
+	var arrplane = surfTool.commit()		#prendo i punti del piano inizializzato
+	
+	var meshtool = MeshDataTool.new()
+	meshtool.create_from_surface(arrplane,0)		#glieli passo da gestire a meshtool
+
+	for i in range(meshtool.get_vertex_count()):		#prendo y da rumore
+		var v = meshtool.get_vertex(i)
+		v.y = noise.get_noise_2d(v.x,v.z) *30
+		meshtool.set_vertex(i, v)
+	
+	for i in range(arrplane.get_surface_count()):		#rimuovo superfici create all'inizializzazione del piano per farne altre
+		arrplane.surface_remove(i)
+
+	meshtool.commit_to_surface(arrplane)
+	
+	surfTool.begin(mesh.PRIMITIVE_TRIANGLES)		#comincio a estrarre la mesh finale
+	surfTool.create_from(arrplane,0)
+	surfTool.generate_normals()
+	
+	var inst = MeshInstance.new()
+	inst.mesh = surfTool.commit()
+	inst.create_trimesh_collision()
+	
+	#inizializzare sistema dettagli:
+
+	var packer = PackedScene.new()            #per salvare la mappa
+	packer.pack(inst)
+	ResourceSaver.save("res://terra.tscn", packer)
+	add_child(inst)
+
+
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
