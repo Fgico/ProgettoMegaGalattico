6dfe6657bda9d4faf1b917776389cfba496c830d
hccsan <sandra.cc.huang@gmail.com>
2020-10-19 19:54:40 +0200

    kinematicbody

---
+++ Entita/Nemici/KinematicBody.gd
@@ -0,0 +1,18 @@
+extends Combattente
+
+
+onready var nav = get_parent().get_node("Navigation")
+onready var player = get_parent().get_node("player")
+var path = []
+var direzione
+
+func _ready():
+	pass
+
+func _process(delta):
+	var arrivo = nav.get_closest_point(player.global_transform.origin)
+	var partenza = nav.get_closest_point(self.global_transform.origin)
+	path = nav.get_simple_path(partenza, arrivo)
+	direzione = path[1] - path[0]
+	direzione = direzione.normalized() * stats.spd
+	move_and_slide(direzione)
