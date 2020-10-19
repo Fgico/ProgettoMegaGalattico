d9b83b4b6889dee56e359489b3fa72dca0d439ee
Federico Giusti <Fgico@users.noreply.github.com>
2020-10-19 16:34:45 +0200

    ordinato stats

--- Entita/Nemici/classeCombattente.gd
+++ Entita/Nemici/classeCombattente.gd
@@ -2,21 +2,30 @@ extends KinematicBody
 
 class_name  Combattente
 
-var hp = 100
-var mp = 100
-
-var atk = 1
-var def = 1
+var stats = {
+	"hp" : 100,
+	"mp" : 100,
+	"atk" : 1,
+	"def" : 1,
+	"spd" : 1
+}
 
 var element
 onready var spawnAtk = get_node("rotable/spawnAtk") 
+
+func _init(natk=1,ndef=1, nhp = 100, nmp = 100, nspd = 1):
+	stats.atk = natk
+	stats.def = ndef
+	stats.hp = nhp
+	stats.mp = nmp
+	stats.spd = nspd
 
 func attacca(attacco):
 	var attacked = attacco.instance()
 	get_parent().add_child(attacked)
 	attacked.global_transform.origin = spawnAtk.global_transform.origin
-	mp -= attacked.mpCost
-	attacked.danno *= atk
+	stats.mp -= attacked.mpCost
+	attacked.danno *= stats.atk
 
 func hit(danno,element):
-	hp -= danno - def
+	stats.hp -= danno - stats.def
