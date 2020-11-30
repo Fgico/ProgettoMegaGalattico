d0ce815c9711a6b298184f41b5e4db055375f5d1
Federico <Fgico@users.noreply.github.com>
2020-11-30 17:25:38 +0100

    Sto diventando matto

--- Dungeon/Entita/Attacchi/Speciali/fuoco/lanciafiamme.gd
+++
@@ -1,9 +0,0 @@
-extends Attacco
-
-func _ready():
-	iniziaStats(40,1,30,1.2)
-	get_node("RaggioFuoco").emitting = true
-
-func _on_Area_body_entered(body):
-	.hit(body)
-	pass
--- Dungeon/Scene Principali/Control.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-onready var collection = get_node("Dun1/SbarreDun/UIcoins_item")
-
-func _ready():
-	pass
