82048b0d48ac7a137713a82cea49fa5e6b0e3437
razihel96 <momog996@gmail.com>
2020-12-01 22:40:21 +0100

    fixato lag assets

--- DUNGEON/Scene Principali/AnimationPlayer.gd
+++
@@ -1,7 +0,0 @@
-extends AnimationPlayer
-
-onready var personaggio = get_node("player")
-
-func _process(delta):
-	if personaggio.stato == personaggio.Moving:
-		personaggio.anim.play("sword and shield run-loop")
