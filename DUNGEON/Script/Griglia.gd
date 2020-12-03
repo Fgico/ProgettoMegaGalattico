f3b507420097461d3f37667c7a790949c47ec1b9
Federico <Fgico@users.noreply.github.com>
2020-12-03 21:58:32 +0100

    nuke

--- DUNGEON/Script/Griglia.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody
-
-
-
-func _ready():
-	pass
-
-func _process(delta):
-	if $CollisionShape
--- DUNGEON/Script/enemyMaster.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-onready var player = get_parent().get_node("player")
-onready var nav = get_node("Navigation")
-
-func _ready():
-	pass # Replace with function body.
