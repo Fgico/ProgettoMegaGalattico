b898bfa9c6c0a84d162a469bd044cf24b93bba87
emphity1 <dimak821@gmail.com>
2020-12-10 02:02:54 +0100

    Revert "Merge branch 'dima' of https://github.com/Fgico/ProgettoMegaGalattico into dima"
    This reverts commit c59958045e65da7555336962920d06bc4c147d5c, reversing
    changes made to 14f3b4b1671de98587b236e59863f49905bfe9d5.

---
+++ Isola/enemyMaster.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+onready var player = get_parent().get_node("player")
+onready var nav = get_node("Navigation")
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
