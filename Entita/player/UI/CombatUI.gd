361dbf846303e33376cdd3acf7adddeaea4591b2
emphity1 <dimak821@gmail.com>
2020-12-20 18:34:15 +0100

    aggiunte impostazioni, spawn(da fixare alla morte)

--- Entita/player/UI/CombatUI.gd
+++ Entita/player/UI/CombatUI.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var inventoryScreen = preload("res://Interfacce Utente/Inventario/InventoryUI.tscn")
+var menu_sett = preload("res://Interfacce Utente/Impostazioni e salvataggi cloud/Settings.tscn")
 
 onready var player = get_parent().get_parent().get_parent().get_parent()
 onready var pauseMenu = get_node("PauseMenu")
@@ -89,3 +90,19 @@ func _on_bag_pressed():
 	player.add_child(inv)
 	player.UI.hide()
 	player.lockMovement = true
+
+
+func _on_Settings_pressed():
+	var settings = menu_sett.instance()
+	self.add_child(settings)
+	player.UI.show()
+	player.lockMovement = true
+	pass # Replace with function body.
+
+
+func _on_back2town_pressed():
+	
+	get_tree().root.get_node("nodo_isola").cambioPiano("res://Isola/ISOLA_VOLANTE/Isola_volante.tscn")
+	
+	
+	pass # Replace with function body.
