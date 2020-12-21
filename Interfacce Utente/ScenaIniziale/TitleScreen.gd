c110170c54412da0f54d360f327249b53c3698ea
hccsan <sandra.cc.huang@gmail.com>
2020-12-21 17:36:40 +0100

    updated change scenes

--- Interfacce Utente/ScenaIniziale/TitleScreen.gd
+++ Interfacce Utente/ScenaIniziale/TitleScreen.gd
@@ -1,15 +1,26 @@
 extends Control
 
-var scene_path_to_load
+var settings = preload("res://Interfacce Utente/Impostazioni e salvataggi cloud/Settings.tscn")
+var cloud = preload("res://Interfacce Utente/Impostazioni e salvataggi cloud/CloudSaveManager.tscn")
 
-func _ready():
-	for button in $Menu/CenterRow/Buttons.get_children():
-		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+#func _ready():
+#	for button in $Menu/CenterRow/Buttons.get_children():
+#		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+	
 
-func _on_Button_pressed(scene_to_load):
-	scene_path_to_load = scene_to_load
+func _on_SettingsButton_pressed():
+	var setting_scene = settings.instance()
+	self.add_child(setting_scene)
+
+
+func _on_ContinueButton_pressed():
 	$FadeIn.show()
 	$FadeIn.fade_in()
+	
 
 func _on_FadeIn_fade_finished():
-	get_tree().change_scene(scene_path_to_load)
+	get_tree().change_scene("res://nodo_isola.tscn")
+
+func _on_CloudButton_pressed():
+	var cloud_scene = cloud.instance()
+	self.add_child(cloud_scene)
