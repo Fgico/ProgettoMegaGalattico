ac82349c2af1bf94f463a604f2e8050ba66b23e7
Federico <Fgico@users.noreply.github.com>
2020-12-22 23:13:02 +0100

    aggiunto equip screen

--- Interfacce Utente/Inventario/playerStats.gd
+++ Interfacce Utente/Inventario/playerStats.gd
@@ -1,8 +1,8 @@
-extends VBoxContainer
+extends Control
 
-onready var atkval = get_node("Attack/StatsValue")
-onready var defval = get_node("Defense/StatsValue")
-onready var atkspdval = get_node("AtkSpeed/StatsValue")
+onready var atkval = get_node("StatsValue")
+onready var defval = get_node("StatsValue2")
+onready var atkspdval = get_node("StatsValue3")
 
 func updatePlayerStats():
 	atkval.text = str(sceneUtili.player.stats.atk)
