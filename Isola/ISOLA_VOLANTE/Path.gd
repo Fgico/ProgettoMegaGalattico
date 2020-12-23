4c445a05aa40abfbc00baec007703ea179418ae3
emphity1 <dimak821@gmail.com>
2020-12-23 12:38:51 +0100

    aggiornato npc

--- Isola/ISOLA_VOLANTE/Path.gd
+++ Isola/ISOLA_VOLANTE/Path.gd
@@ -1,17 +1,11 @@
 extends Path
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-onready var PathFollow = get_node("PathFollow")
+onready var follow = get_node("PathFollow")
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _process(delta):
-	PathFollow.set_offset(PathFollow.get_offset()+4*delta)
+	follow.set_offset(follow.get_offset() + 3 * delta)
