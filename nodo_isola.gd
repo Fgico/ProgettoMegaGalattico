c65138679bc2d19541a8b93f7e3e699b8e038c93
Federico <Fgico@users.noreply.github.com>
2020-12-23 22:15:02 +0100

    added city and dungeon music

--- nodo_isola.gd
+++ nodo_isola.gd
@@ -3,6 +3,9 @@ extends Spatial
 onready var pianoAttuale = get_node("Isola_volante")
 onready var player = get_node("player")
 
+onready var musicaCitta = get_node("musicaCitta")
+onready var musicaDun = get_node("musicaDun")
+
 func _ready():
 	pass # Replace with function body.
 
@@ -12,6 +15,12 @@ func cambioPiano(nuovoLivello):
 	pianoAttuale.queue_free()
 	var pianoCreato = newPiano.instance() #creo il nuovo piano
 	self.add_child(pianoCreato)
+	if(nuovoLivello == "res://Isola/ISOLA_VOLANTE/Isola_volante.tscn"):
+		musicaCitta.playing = true
+		musicaDun.playing = false
+	else:
+		musicaCitta.playing = false
+		musicaDun.playing = true
 	pianoAttuale = pianoCreato
 	get_node("player/target/Camera").current = true
 	userData.saveToFile()
